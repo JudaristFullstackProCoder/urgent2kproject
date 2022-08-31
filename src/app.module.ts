451ea090f1118befab444a4c92787f8a78027787
Jude <judearist@gmail.com>
2022-08-31 20:34:19 +0100

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CountriesController } from './countries/countries.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { CountriesController } from './countries/countries.controller';
       isGlobal: true,
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController, CountriesController],
   providers: [AppService],
