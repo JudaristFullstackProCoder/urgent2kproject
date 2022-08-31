8ad12b29b70c81ae797f80c6f83b2c0155dad099
Jude <judearist@gmail.com>
2022-08-31 18:32:23 +0100

    create controller country

---
+++ src/countries/countries.controller.ts
@@ -0,0 +1,63 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { countries, continents, languagesAll } from 'countries-list'
+import * as countriesCities from 'country-city';
+import Continent from './continent';
+import Country from './country';
+import CountryCities from './countryCities';
+import Language from './language';
+
+@Controller('')
+@ApiTags('Countries')
+export class CountriesController {
+    @Get('countries')
+    @ApiOkResponse({
+      type: Country,
+      description: 'return a list of all available countries',
+      status: 200,
+      isArray: false,
+    })
+    async getAllCountries(
+    ) {
+      return { ...countries}
+    }
+
+    @Get('cities')
+    @ApiOkResponse({
+      type: CountryCities,
+      description: 'return a list of all available countries',
+      status: 200,
+      isArray: false,
+    })
+    async getCitiesOfCountry(
+        @Query('c') countryName: string,
+    ) {
+      return {
+          cities: countriesCities.getCities(countryName)
+      }
+    }
+
+    @Get('continents')
+    @ApiOkResponse({
+      type: Continent,
+      description: 'return a list of all available countries',
+      status: 200,
+      isArray: false,
+    })
+    async getAllContinents(
+    ) {
+      return { ...continents}
+    }
+
+    @Get('languages')
+    @ApiOkResponse({
+      type: Language,
+      description: 'return a list of all available countries',
+      status: 200,
+      isArray: false,
+    })
+    async getAllLanguages(
+    ) {
+      return { ...languagesAll}
+    }
+}
