32ff56343c5f2c7e472738b6f3bcfe6445367845
Jude <judearist@gmail.com>
2022-08-31 23:33:09 +0100

    remove navbar

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,11 +1,9 @@
 import { Header } from '../component/header';
-import { NavbarMinimal } from '../component/navbar';
 
 export default function Home({ setTheme, themeColor }) {
   return (
     <div>
       <Header setTheme={setTheme} themeColor={themeColor} />
-      <NavbarMinimal />
     </div>
   );
 }
