0b6d3603d49aa36ba4c6116b9c4c7e5d96673ce4
Jude <judearist@gmail.com>
2022-08-31 18:31:40 +0100

    create langùage class to improve api doc

---
+++ src/countries/language.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger"
+
+export default class Language {
+    /**
+     * Language name in English.
+     */
+    @ApiProperty({
+        example: 'French',
+    })
+    name: string
+    /**
+     * Language name written natively.
+     */
+     @ApiProperty({
+        example: 'Français',
+    })
+    native: string
+    /**
+     * Specified if Language is RTL.
+     */
+     @ApiProperty({
+        example: null,
+    })
+    rtl?: number
+  }
